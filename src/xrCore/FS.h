2ae89d30553bb70857f4365e510d6f3932f0b0ac
OneMorePseudoCoder <73041211+OneMorePseudoCoder@users.noreply.github.com>
2026-03-01 21:33:54 +0300

    Фикс сборки на новейшей версии MVS
    Отсюда: https://github.com/abramcumner/xray16/commit/f55d3ead28272d166173dd152d3a12384b47f60b

--- src/xrCore/FS.h
+++ src/xrCore/FS.h
@@ -214,7 +214,7 @@ public:
     // Set file pointer to start of chunk data (0 for root chunk)
     IC void rewind() { impl().seek(0); }
 
-    u32 find_chunk(u32 ID, BOOL* bCompressed);
+    IC u32 find_chunk(u32 ID, BOOL* bCompressed);
 
     IC BOOL r_chunk(u32 ID, void* dest) // чтение XR Chunk'ов (4b-ID,4b-size,??b-data)
     {
@@ -242,6 +242,8 @@ public:
 private:
     u32 m_last_pos;
 };
+
+#include "FS_impl.h"
 
 class XRCORE_API IReader : public IReaderBase < IReader >
 {
