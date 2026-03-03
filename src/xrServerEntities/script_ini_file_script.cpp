46609164b6a14dcc8d3142f7227ff5894f5b5071
OneMorePseudoCoder <73041211+OneMorePseudoCoder@users.noreply.github.com>
2026-03-03 22:17:46 +0300

    Скриптовые экспорты 2
    Отсюда: https://github.com/revolucas/CoC-Xray/commit/c9121c566e6592abe7c41b3008a1bb15c2b12ce1

--- src/xrServerEntities/script_ini_file_script.cpp
+++ src/xrServerEntities/script_ini_file_script.cpp
@@ -13,8 +13,19 @@ using namespace luabind;
 
 CScriptIniFile *get_system_ini()
 {
-	return	((CScriptIniFile*)pSettings);
+	return ((CScriptIniFile*)pSettings);
 }
+
+//Alundaio: The extended ability to reload system ini after application launch
+CScriptIniFile *reload_system_ini()
+{
+	pSettings->Destroy(const_cast<CInifile*>(pSettings));
+	string_path fname;
+	FS.update_path(fname, "$game_config$", "system.ltx");
+	pSettings = xr_new<CInifile>(fname);
+	return ((CScriptIniFile*)pSettings);
+}
+//Alundaio: END
 
 #ifdef XRGAME_EXPORTS
 CScriptIniFile *get_game_ini()
@@ -46,16 +57,7 @@ bool r_line(CScriptIniFile *self, LPCSTR S, int L,	luabind::internal_string &N, 
 #pragma warning(disable:4238)
 CScriptIniFile *create_ini_file	(LPCSTR ini_string)
 {
-	return			(
-		(CScriptIniFile*)
-		xr_new<CInifile>(
-			&IReader			(
-				(void*)ini_string,
-				xr_strlen(ini_string)
-			),
-			FS.get_path("$game_config$")->m_Path
-		)
-	);
+	return ((CScriptIniFile*)xr_new<CInifile>(&IReader((void*)ini_string, xr_strlen(ini_string)), FS.get_path("$game_config$")->m_Path));
 }
 #pragma warning(pop)
 
@@ -66,6 +68,26 @@ void CScriptIniFile::script_register(lua_State *L)
 	[
 		class_<CScriptIniFile>("ini_file")
 			.def(					constructor<LPCSTR>())
+			//Alundaio: Extend script ini file
+			.def("w_bool",&CScriptIniFile::w_bool)
+			.def("w_color", &CScriptIniFile::w_color)
+			.def("w_fcolor", &CScriptIniFile::w_fcolor)
+			.def("w_float", &CScriptIniFile::w_float)
+			.def("w_vector2", &CScriptIniFile::w_fvector2)
+			.def("w_vector", &CScriptIniFile::w_fvector3)
+			.def("w_s16", &CScriptIniFile::w_s16)
+			.def("w_s32", &CScriptIniFile::w_s32)
+			.def("w_s64", &CScriptIniFile::w_s64)
+			.def("w_s8", &CScriptIniFile::w_s8)
+			.def("w_string", &CScriptIniFile::w_string)
+			.def("w_u16", &CScriptIniFile::w_u16)
+			.def("w_u32", &CScriptIniFile::w_u32)
+			.def("w_u64", &CScriptIniFile::w_u64)
+			.def("w_u8", &CScriptIniFile::w_u8)
+			.def("save_as", (bool (CScriptIniFile::*)(LPCSTR))(&CScriptIniFile::save_as))
+			.def("save_at_end", &CScriptIniFile::save_at_end)
+			.def("remove_line", &CScriptIniFile::remove_line)
+			//Alundaio: END
 			.def("section_exist",	&CScriptIniFile::section_exist	)
 			.def("line_exist",		&CScriptIniFile::line_exist		)
 			.def("r_clsid",			&CScriptIniFile::r_clsid		)
@@ -81,6 +103,9 @@ void CScriptIniFile::script_register(lua_State *L)
 			.def("r_line",			&::r_line, out_value<4>() + out_value<5>()),
 
 		def("system_ini",			&get_system_ini),
+		//Alundaio: extend
+		def("reload_system_ini",    &reload_system_ini),
+		//Alundaio:: END
 #ifdef XRGAME_EXPORTS
 		def("game_ini",				&get_game_ini),
 #endif // XRGAME_EXPORTS
